d57e7c9f39cba45b442649f5bb87d7c486d85e7a
prakashtaxlaws <prakash.k@taxlawsonline.com>
2024-11-05 09:45:21 +0530

    Added UI

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileDetails from "./ProfileDetails";
+import Login from "./login";
+import ProfileCards from "./ProfileCards";
+
+export default function App() {
+  return (
+    <div className="bg-slate-100 ">
+      {/* <Login /> */}
+      <ProfileCards />
+      {/* <ProfileDetails /> */}
+    </div>
+  );
+}
