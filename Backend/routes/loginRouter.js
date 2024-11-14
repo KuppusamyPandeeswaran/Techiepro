bd769b30a906a4ad3746f53ecc813fc499c58624
prakashtaxlaws <prakash.k@taxlawsonline.com>
2024-11-14 17:05:51 +0530

    Removed Backed

--- Backend/routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { techie_login } = require('../controllers/login');
-
-router.route('/').get(techie_login);
-
-module.exports = router;
--- Backend/routes/techieRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { get_single_techie, get_all_techie } = require('../controllers/techie');
-
-router.route('/').get(get_all_techie);
-router.route('/:id').get(get_single_techie);
-
-module.exports = router;
