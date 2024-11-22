b2dee3ecc1f516c099219436a560f432dff68fc6
KuppusamyPandeeswaran <pandeeswarankuppusamy@gmail.com>
2024-11-22 10:54:16 +0530

    profile change dated:22-11-2024

--- Frontend/src/ProfileDetails.jsx
+++ Frontend/src/ProfileDetails.jsx
@@ -4,14 +4,21 @@ import Navbar from "./Navbar";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLink } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProfileDetails() {
+  const location = useLocation();
+  const { userdata } = location.state;
   return (
     <>
-      <div className="bg-[#f0faff]">
+      <div key={userdata.username} className="bg-[#f0faff]">
         <Navbar />
-        <div className="pt-16 pb-8 px-8">
+        <div className="pt-16 pb-8 px-8 ">
           <h1 className="text-3xl font-semibold">Profile</h1>
+          {/* <a href="" className="text-xl ">
+            Edit
+          </a> */}
         </div>
         <div className="parent-grid pb-20 grid grid-cols-3 gap-2 ">
           {/* Left section */}
@@ -21,20 +28,20 @@ export default function ProfileDetails() {
               alt=""
               className="w-24 h-24 object-cover rounded-lg"
             />
-            <h1 className="text-lg mt-2 font-semibold">Prakash</h1>
-            <p className="text-sm my-1">Software Engineer</p>
+            <h1 className="text-lg mt-2 font-semibold">{userdata.username}</h1>
+            <p className="text-sm my-1">{userdata.role}</p>
             <p className="text-sm my-1">Chennai</p>
             <div className="my-4">
               <h3 className="text-sm font-semibold">Email Address</h3>
-              <p>prakash.k@taxlawsonline.com</p>
+              <p>{userdata.email}</p>
             </div>
             <div className="my-4">
               <h3 className="text-sm font-semibold">Address</h3>
-              <p>New Perungalathur, Chennai</p>
+              <p>{userdata.address}</p>
             </div>
             <div className="my-4">
               <h3 className="text-sm font-semibold">Mobile Number</h3>
-              <p>+91 7845151962</p>
+              <p>{userdata.mobile}</p>
             </div>
             <h1 className="text-lg font-bold mb-4">Socials</h1>
 
@@ -62,8 +69,16 @@ export default function ProfileDetails() {
           <div className="bio-section px-6 pt-4 border-2 border-[#D0E7F5] rounded-lg border-solid mr-8 second-grid pb-8 col-span-2 ">
             <div className="grid grid-cols-2 gap-4">
               <div className="div1 col-span-2 mb-6">
-                <h1 className="text-lg font-semibold mb-4">Bio</h1>
+                <div className="flex justify-between">
+                  <h1 className="text-lg font-semibold mb-4">Bio</h1>
 
+                  <Link
+                    to={"/ProfileEdit"}
+                    className="text-[12px] text-[#00b0ff] underline hover:no-underline"
+                  >
+                    Edit Profile
+                  </Link>
+                </div>
                 <p>
                   {" "}
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -78,7 +93,7 @@ export default function ProfileDetails() {
               </div>
               <div className="div3 ">
                 <h3 className="text-sm">Role</h3>
-                <p>Software Engineer</p>
+                <p>{userdata.role}</p>
               </div>
               <div className="div4">
                 <h3 className="text-sm">Languages</h3>
@@ -98,88 +113,20 @@ export default function ProfileDetails() {
                 <p>Software Engineer at Company Law Insitute of India</p>
               </div>
             </div>
-            <h1 className="text-lg font-bold mb-4">Skills</h1>
-            <div className="skill-list flex flex-wrap gap-2 ">
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm hover:text-white hover:bg-[#00b0ff] transition-colors">
-                HTML
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                CSS
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                JavaScript
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Bootstrap
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Tailwind
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Git
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Angular
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                XML
-              </p>
+            <h1 className="text-lg font-bold mb-4 mt-8">Skills</h1>
+            <div>
+              <div className="skill-list flex flex-wrap gap-2 ">
+                {userdata.skills.map((skill, index) => (
+                  <p
+                    className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm hover:text-white hover:bg-[#00b0ff] transition-colors"
+                    key={index}
+                  >
+                    {skill}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
-          {/* Bottom left section  */}
-          {/* <div className="bio-section px-6 pt-4 border-2 border-[#D0E7F5] rounded-lg border-solid ml-8 third-grid pb-8 col-span-1 ">
-            <h1 className="text-lg font-bold mb-4">Socials</h1>
-
-            <ul className="flex gap-4">
-              <li>
-                <a href="">
-                  <FaLinkedinIn className="w-6 h-6" color="#0e76a8" />
-                </a>
-              </li>
-              <li>
-                {" "}
-                <a href="">
-                  <FaGithub className="w-6 h-6" color="#0e76a8" />
-                </a>
-              </li>
-              <li>
-                {" "}
-                <a href="">
-                  <FaLink className="w-6 h-6" color="#0e76a8" />
-                </a>
-              </li>
-            </ul>
-          </div> */}
-          {/* Bottom right section */}
-          {/* <div className="bio-section px-6 pt-4 border-2 border-[#D0E7F5] rounded-lg border-solid mr-8 fourth-grid pb-8 col-span-2">
-            <h1 className="text-lg font-bold mb-4">Skills</h1>
-            <div className="skill-list flex flex-wrap gap-2 ">
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm hover:text-white hover:bg-[#00b0ff] transition-colors">
-                HTML
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                CSS
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                JavaScript
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Bootstrap
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Tailwind
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Git
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                Angular
-              </p>
-              <p className=" border-2 border-[#00b0ff] rounded-lg py-1 px-2 text-sm  hover:text-white hover:bg-[#00b0ff] transition-colors">
-                XML
-              </p>
-            </div>
-          </div> */}
         </div>
       </div>
     </>
